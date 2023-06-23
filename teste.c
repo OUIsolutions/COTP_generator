fbf90992bffbe9e96db441e0fa215f6b34f2e952
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-06-22 21:59:19 -0300

    att

--- teste.c
+++ teste.c
@@ -8,7 +8,7 @@
 int main(){
     printf("----------- Key Construction ----------------\n");
     const char *secreet = "akisjioajsidjioasidaisr";
-    int interval = ctops_days(365);
+    int interval = ctops_days(765);
     CtopKeyAttributes  key_attribtes = newCtopKeyAttribute(
             interval,
             secreet,
