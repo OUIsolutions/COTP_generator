d208b7c9de3888aa27d549474d753084f3e3e1fd
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-06-23 17:26:53 -0300

    att

--- build.py
+++ build.py
@@ -4,11 +4,13 @@ EXEMPLES = 'exemples'
 OUTPUT = 'COTPgenerator.h'
 STARTER = f'COTP/COTP_main.h'
 
+
 def execute_copilation(file:str,remove_output:bool):
     try:
         output = ct.compile_project('gcc',file)
         if remove_output:
             remove(output)
+        return output
     except ct.CopilationWarning as e:
         remove(output)
         raise e
@@ -16,8 +18,24 @@ def execute_copilation(file:str,remove_output:bool):
     except Exception as e:
         raise e
 
+def test_execution(file:str,expected_output:str):
+    output = execute_copilation(file,False)
+    try:
+        result = ct.ComandLineExecution(output)
+    except Exception as e:
+        remove(output)
+        raise e
+    remove(output)
+
+    if result.output.strip() != expected_output:
+        raise Exception(f'output of {file} diferent')
+    print(f'passed: {file}')
+
 def test_static_generation():
-    pass
+    test_execution(
+        f'{EXEMPLES}/static_generation.c',
+        '94adef2de017bea53aa810a8a08ddd1c277bd93b63ce0e1ad48ea4bd24ecc30100000060201'
+    )
 
 
 def test_exemples():
@@ -26,6 +44,7 @@ def test_exemples():
     print("passed: key_generation.c")
     execute_copilation(f'{EXEMPLES}/password_generation.c',True)
     print('passed: password_generation.c')
+    test_static_generation()
 
 test_exemples()
 amalgamated_code = ct.generate_amalgamated_code(STARTER,OUTPUT)
---
+++ exemples/static_generation.c
@@ -0,0 +1,23 @@
+//
+// Created by jurandi on 23-06-2023.
+//
+
+#include "COTPgenerator.h"
+
+int main(){
+
+    //Not share these in any public repo
+    const char *secreet = "jisduohfuhusdh8q0j023233321";
+    //the interval betewen password changing
+    int interval = cotp_minutes(1);
+    long now = 1;
+    COTPKeyAttributes  key_attribtes = newCOTPKeyAttribute(
+            interval,
+            secreet,
+            now
+    );
+    char key[COTP_MAX] = {0};
+    cotp_create_key_string_from_attributes(key, &key_attribtes);
+    printf("%s\n", key);
+    return 0;
+}
