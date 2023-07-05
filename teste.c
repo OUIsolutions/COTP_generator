f35dae5ed20892531d52682c8b0718b79064dd14
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-05 03:00:45 -0300

    att

--- teste.c
+++ teste.c
@@ -1,22 +1,21 @@
-//
-// Created by jurandi on 23-06-2023.
-//
-
-#include "COTP/COTP_main.h"
+#include "COTPgenerator.h"
 
 
 int main(){
 
-    const char *key = "93872169427172161923161239171771822141200001800100";
+    const char *key = "6fb07055e72bbe50e1a5f4ffe1ff787d7dcb037940a49e7dfbbea87567ff6fee00000060100";
 
     char password[COTP_MAX]  = {0};
     int time_remaning;
 
+    while(true){
+        system("clear");
 
-    long now = 1;
-    cotp_get_password(password,&time_remaning,key,now);
-    printf("password: %s\n",password);
-    printf("time remaning :%d\n",time_remaning);
-    sleep(1);
-
+        long now = time(NULL);
+        cotp_get_password(password,&time_remaning,key,now);
+        printf("password: %s\n",password);
+        printf("time remaning :%d\n",time_remaning);
+        sleep(1);
+    }
 }
+//6fb07055e72bbe50e1a5f4ffe1ff787d7dcb037940a49e7dfbbea87567ff6fee00000060100
